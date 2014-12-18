d7a100156369f2d08446d024cd44c1be94e6fe67
BLDSBE <daniel.wassermann@student.uni-tuebingen.de>
2014-12-18 17:02:17 +0100

    Create Bsp.java
    Einfaches Beispiel, das unser Compiler am Ende schaffen muss. Alles weitere würde ich vorschlagen machen wir, wenn dieser Teil funktioniert.

---
+++ Bsp.java
@@ -0,0 +1,26 @@
+package compilerbau;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Compilerbau {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 0;
+        boolean a = true;
+        char b = 't';
+        
+        if (a)
+            i = 5;
+        else
+            b = 'f';
+        
+        System.out.println("bool war "+ b +", deshalb ist i = "+i);
+        
+    }
+    
+}
