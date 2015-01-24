bdd451eabaa44b9c7b4111777cd84c718b506fc4
Lukas <lukas.hollaender@student.uni-tuebingen.de>
2015-01-24 13:01:50 +0100

    =Constants tatsächlich da.

---
+++ Constants.hs
@@ -0,0 +1,33 @@
+module Constants where
+
+arrayTypePrefix	= "[" -- unused so far, may be used for array literals
+arrayTypePostfix	= ";"
+voidType 			= "V"
+booleanType 		= "Z"
+charType 			= "C"
+intType 				= "I"
+stringType 			= "Ljava/lang/String"
+nullType 			= "NullType"
+objectType 			= "java/lang/Object"
+
+thisString 			= "this"
+superString 		= "super"
+
+shiftL				= "<<"
+shiftR2 				= ">>"
+shiftR3 				= ">>>"
+multiplication 	= "*"
+division 			= "/"
+modulo 				= "%"
+lessThan 			= "<"
+greaterThan 		= ">"
+lessOrEqual 		= "<="
+greaterOrEqual 	= ">="
+instanceOf 			= "instanceof"
+equals 				= "=="
+unequal 				= "!="
+and1 					= "&"
+and2 					= "&&"
+or1 					= "|"
+or2 					= "||"
+xor 					= "^"
